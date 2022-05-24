948a62b1b7a389675cac95f1e3f00d549cd32b57
lkdubonc <102050221+lkdubonc@users.noreply.github.com>
2022-05-24 10:22:40 -0600

    Probando alternativa de API

---
+++ js/crm-jsonserver/API.js
@@ -0,0 +1,67 @@
+const url = 'https://my-json-server.typicode.com/lkdubonc/lkdubonc.github.io/clientes';
+
+// Nuevo cliente
+export const nuevoCliente = async cliente => {
+    try {
+        const resultado = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Elimina un cliente
+export const eliminarCliente = async (id) => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Obtiene un cliente por su id
+export const obtenerCliente = async id => {
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Act registro
+export const editarCliente = async cliente => {
+    try {
+        await fetch(`${url}/${cliente.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+}
